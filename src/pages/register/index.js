7944f256b608604fc1bd1e76df3f38287cb4130e
RafaelAbath <laurentino.silva@escolar.ifrn.edu.br>
2024-01-04 14:53:16 -0300

    atualização paginas

---
+++ src/pages/register/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const Registro = () => {
+    const [email, setEmail] = useState("");
+    const [nomeCadastro, setNome] = useState("");
+    const [senhaCadastro, setSenha] = useState("");
+    const [confirmSenha, setConfirmSenha] = useState("");
+
+    function cadastrarUsuario (email, nomeCadastro, senhaCadastro) {
+        let usuario = {email, nomeCadastro, senhaCadastro};
+        localStorage.setItem("usuario", JSON.stringify (usuario));
+    }
+
+    function validacaoSenha (senhaCadastro, confirmSenha) {
+        if( senhaCadastro!=confirmSenha){
+            alert("As senhas não coincidem.")
+        }
+        else {
+            cadastrarUsuario(email, nomeCadastro, senhaCadastro)
+        }
+    }
+
+    return (
+        <div>
+            <h2>Faça seu Cadastro!</h2>
+            <form action="">
+                <label>E-Mail:</label>
+                <input type="email" 
+                value={email}
+                onChange={(e)=> setEmail(e.target.value)}
+                />
+
+                <label>Nome:</label>
+                <input type="text" 
+                value={nomeCadastro}
+                onChange={(e)=> setNome(e.target.value)}
+                />
+
+                <label>Senha:</label>
+                <input type="password" 
+                value={senhaCadastro}
+                onChange={(e)=> setSenha(e.target.value)}
+                />
+
+                <label>Confirmar Senha:</label>
+                <input type="password" 
+                value={confirmSenha}
+                onChange={(e)=> setConfirmSenha(e.target.value)}
+                />
+                <input type="button" onClick={()=>validacaoSenha(senhaCadastro, confirmSenha)} value="Cadastrar Usuário" />
+            </form>
+            
+        </div>
+    )
+}
+export default Registro;
--- src/pages/sigin/index.js
+++ src/pages/sigin/index.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./sigin.module.css";
 
-const login = () => {
-  console.log(login);
-  return <div>login</div>;
-};
+const Login = () => {
+  const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
 
-export default login;
+    function validacao () {
+      let usuario = JSON.parse(localStorage.getItem('usuario'))
+      console.log(usuario)
+
+      let emailCadastrado = usuario.email
+      let senhaCadastrada = usuario.senhaCadastro
+  
+      if(emailCadastrado === email && senhaCadastrada === senha) {
+          alert("Logado com Sucesso!! Aguarde que em breve você será redirecionado para a página de destino.")
+          
+      } else {
+          alert("E-mail ou senha incorretos!")
+      }
+  }
+
+  return (<div>
+  <h2>Faça seu Login!</h2>
+  <form >
+      <label >E-Mail: </label>
+      <input type="email" 
+      value={email}
+      onChange={(e)=> setEmail(e.target.value)}
+      required
+      />
+
+      <label>Senha: </label>
+      <input type="password" 
+      value={senha}
+      onChange={(e)=> setSenha(e.target.value)}
+      required
+      />
+    <input type="button" onClick={()=>validacao()} value="Login" />
+  </form>
+
+</div>
+)};
+
+export default Login;
